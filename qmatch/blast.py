14b3af2ccf3bed70ce617bdfb705f668806e18ef
David McClure <davidwilliammcclure@gmail.com>
2016-10-20 12:39:12 -0700

    Shelve BLAST index.

--- qmatch/blast.py
+++ qmatch/blast.py
@@ -1,13 +1,14 @@
 
 
 import re
+import shelve
 
+from collections import defaultdict
 from typing import List
 from hirlite import Rlite
 from vedis import Vedis
 from boltons.iterutils import windowed_iter
 from spooky import hash32
-from collections import defaultdict
 
 
 class BlastRlite:
@@ -111,5 +112,44 @@ class BlastMem:
         """
 
         key = hash32('.'.join(ngram))
+
+        return self.index.get(key)
+
+
+class BlastShelve:
+
+    def __init__(self, path: str):
+
+        """
+        Set the dictionary.
+        """
+
+        self.index = shelve.open(path)
+
+    def index_text(self, text: str, identifier: str, n: int):
+
+        """
+        Index N-grams for a text.
+        """
+
+        tokens = re.findall('[a-z]+', text.lower())
+
+        for phrase in windowed_iter(tokens, n):
+
+            key = str(hash32('.'.join(phrase)))
+
+            val = self.index.get(key, set())
+
+            val.add(identifier)
+
+            self.index[key] = val
+
+    def lookup(self, ngram: List[str]):
+
+        """
+        Get text ids for an ngram.
+        """
+
+        key = str(hash32('.'.join(ngram)))
 
         return self.index.get(key)
